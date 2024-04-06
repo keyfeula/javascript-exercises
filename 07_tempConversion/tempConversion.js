b843c28cc4a2ef705c03d944eec7d0ffe1204fd7
Key Feula <keyfeula1214@gmail.com>
2024-04-06 16:37:52 -0700

    Fix celsius and fahrenheit conversion functions to pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    //(temp in fahrenheit - 32) * 5/9
+    let celsius = (temp - 32) * 5 / 9;
+    return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    //(temp in celsius * 9/5) + 32
+    temp = Number(temp);
+    let fahrenheit = (temp * 9 / 5) + 32;
+    return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
