242cb307273376e83c7fd1c328226ef0cc507925
Key Feula <keyfeula1214@gmail.com>
2024-04-06 13:57:40 -0700

    Fix removeFromArray function using indexOf and splice to pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numsToRemove) {
+    for (const num of numsToRemove) {
+        index = array.indexOf(num);
+        while (index != -1) {
+            array.splice(index, 1);
+            index = array.indexOf(num);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
