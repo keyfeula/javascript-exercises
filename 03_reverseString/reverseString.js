aef28175c56a3dfa7dd0722ed312844f4a7f5422
Key Feula <keyfeula1214@gmail.com>
2024-04-06 12:26:27 -0700

    Fix reverseString method to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    length = string.length;
+    const reverse = [];
+    for (let i = length - 1; i >= 0; i--) {
+        reverse.push(string.at(i));
+    }
+    return reverse.join("");
 };
 
 // Do not edit below this line
