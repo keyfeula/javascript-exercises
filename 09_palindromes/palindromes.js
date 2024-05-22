300214c859e95aba5bfc461ee3cea971355403f7
Key Feula <keyfeula1214@gmail.com>
2024-05-22 16:07:26 -0700

    Complete function that checks if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let formattedStr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => alphanumerical.includes(char))
+        .join();
+    let reversedStr = formattedStr
+        .slice()
+        .split("")
+        .reverse()
+        .join("");
+    console.log(`rev: ${reversedStr} str: ${formattedStr}`);
+    return formattedStr === reversedStr;
 };
 
 // Do not edit below this line
