7bb76efc26597d12d55dc509b0c2f72fd64b97c0
Key Feula <keyfeula1214@gmail.com>
2024-05-22 16:41:58 -0700

    Complete function for getting the nth number of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) {
+        return "OOPS";
+    }
+    //fib(4) = 3;    1 1 2 3 5
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
