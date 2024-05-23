14c18967814e99220483eab87fb271bb5469a61e
Key Feula <keyfeula1214@gmail.com>
2024-05-22 17:40:14 -0700

    Complete function for determing oldest person in an array of people objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = arr[0];
+    let oldestAge = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let current = arr[i];
+        let age = 0;
+        if ("yearOfDeath" in current) {
+            age = current.yearOfDeath - current.yearOfBirth;
+        }
+        else {
+            age = new Date().getFullYear() - current.yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldest = arr[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
